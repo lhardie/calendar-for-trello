8a958a98d085b0f68dbaf20cdd7b38ed2a5a345a
Jan Blankenhorn <blankenhorn@weiglewilczek.com>
2016-09-05 10:25:20 +0200

    rested initService from TypeScript to JavaScript

--- src/initModules.ts
+++ src/initModules.ts
@@ -4,5 +4,4 @@ import "./home/HomeService";
 import "./home/HomeController";
 import "./services/setToken";
 import "./services/changeDate";
-import "./services/setToken";
 import "./js/trelloCal.config";
