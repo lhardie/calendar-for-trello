021044cc1a647b6e873b17dda07373ef2904bb09
Jan Blankenhorn <blankenhorn@weiglewilczek.com>
2016-09-09 12:19:09 +0200

    more on months

--- src/route/month/month.ts
+++ src/route/month/month.ts
@@ -5,6 +5,8 @@ import moment from 'moment'
 import {appModule} from "../../app";
 import {CalService} from "../../services/CalService";
 import {ChangeDateService} from "../../services/changeDate";
+import SortableEvents = angular.ui.SortableEvents;
+import SortableOptions = angular.ui.SortableOptions;
 
 var month = angular.module('trelloCal.month', []);
 month.config(/*ngInject*/ function (toastrConfig) {
@@ -67,7 +69,7 @@ class MonthController {
 
 
     constructor(private $interval: ng.IIntervalService, private toastr,
-                $scope, private CalService: CalService, private changeDate: ChangeDateService,
+                $scope: ng.IScope, private CalService: CalService, private changeDate: ChangeDateService,
                 private $window: ng.IWindowService,
                 private orderByFilter, private ngProgress, private initService, private $q: ng.IQService,
                 private $rootScope: ng.IRootScopeService,
@@ -155,50 +157,52 @@ class MonthController {
     };
 
 
-    public sortableOptions() {/*
-     receive: function (e, ui) {
-     var id = ui.item[0].children[1].id.split('-')[0];
-     this.ngProgress.start();
-     var str = e.target.id + ui.item[0].children[1].id.split('-')[1];
-     var newStr = [];
-     angular.forEach(str.split(','), function (value) {
-     newStr.push(parseInt(value));
-     });
-     if (!newStr[3]) {
-     newStr[3] = 12;
-     newStr.push(0);
-     newStr.push(0);
-     }
-     var targetDate = new Date(newStr[0], newStr[1] - 1, newStr[2], newStr[3], newStr[4]);
+    public sortableOptions() {
+        let events: SortableOptions<any> = {
+            receive: function (e, ui) {
+                var id = ui.item[0].children[1].id.split('-')[0];
+                this.ngProgress.start();
+                var str = e.target.id + ui.item[0].children[1].id.split('-')[1];
+                var newStr = [];
+                angular.forEach(str.split(','), function (value) {
+                    newStr.push(parseInt(value));
+                });
+                if (!newStr[3]) {
+                    newStr[3] = 12;
+                    newStr.push(0);
+                    newStr.push(0);
+                }
+                var targetDate = new Date(newStr[0], newStr[1] - 1, newStr[2], newStr[3], newStr[4]);
 
-     this.tempPost.push([id, targetDate]);
-     updateChangeArray();
+                this.tempPost.push([id, targetDate]);
+                this.updateChangeArray();
 
-     },
-     revert: true,
-     placeholder: 'card',
-     connectWith: '.dayCards',
-     over: function (event, ui) {
-     var element = document.getElementById(event.target.id);
-     if (event.target.id !== ui.item[0].parentElement.id) {
-     var children = element.children;
-     _.forEach(children, function (child) {
-     child.style.transform = 'scale(0.8)';
-     });
-     }
-     element.style.borderColor = '#42548E';
-     element.style.borderStyle = 'dashed';
-     element.style.borderWidth = '3px';
+            },
+            revert: true,
+            placeholder: 'card',
+            connectWith: '.dayCards',
+            over: function (event, ui) {
+                var element = document.getElementById(event.target.id);
+                if (event.target.id !== ui.item[0].parentElement.id) {
+                    var children = element.children;
+                    _.forEach(children, function (child) {
+                        child.style.transform = 'scale(0.8)';
+                    });
+                }
+                element.style.borderColor = '#42548E';
+                element.style.borderStyle = 'dashed';
+                element.style.borderWidth = '3px';
 
-     },
-     out: function (event) {
-     var element = document.getElementById(event.target.id);
-     element.style.borderStyle = 'none';
-     var children = element.children;
-     _.forEach(children, function (child) {
-     child.style.transform = 'scale(1)';
-     });
-     }*/
+            },
+            out: function (event) {
+                var element = document.getElementById(event.target.id);
+                element.style.borderStyle = 'none';
+                var children = element.children;
+                _.forEach(children, function (child) {
+                    child.style.transform = 'scale(1)';
+                });
+            }
+        };
     };
 
     private updateChangeArray() {
